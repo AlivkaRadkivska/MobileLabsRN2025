e8f521a4f62279bb755777ddf961a8ae22ab5755
AlivkaRadkivska <aradkivska@gmail.com>
2025-03-11 20:28:58 +0200

    feat: add screen with registration

--- lab_1/app/(tabs)/profile.tsx
+++ lab_1/app/(tabs)/profile.tsx
@@ -1,23 +1,81 @@
-import { Stack } from "expo-router";
+import { useState } from 'react';
+import { ScrollView, Text, TextInput, View } from 'react-native';
+import { Button } from '~/components/Button';
 
-import { StyleSheet, View } from "react-native";
+export default function Profile() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [firstName, setFirstName] = useState('');
 
-import { ScreenContent } from "~/components/ScreenContent";
-
-export default function Home() {
   return (
-    <>
-      <Stack.Screen options={{ title: "Tab Two" }} />
-      <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/two.tsx" title="Tab Two" />
+    <ScrollView className="flex-1">
+      <Text className="text-center text-xl font-semibold">Реєстрація</Text>
+
+      <View className="p-2">
+        <Text className="text-sm">Електронна пошта</Text>
+        <TextInput
+          inputMode="email"
+          className="border border-gray-300"
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+        />
       </View>
-    </>
+
+      <View className="p-2">
+        <Text className="text-sm">Пароль</Text>
+        <TextInput
+          secureTextEntry={true}
+          className="border border-gray-300"
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+        />
+      </View>
+
+      <View className="p-2">
+        <Text className="text-sm">Пароль (ще раз)</Text>
+        <TextInput
+          secureTextEntry={true}
+          className="border border-gray-300"
+          onChangeText={(text) => setPasswordConfirmation(text)}
+          value={passwordConfirmation}
+        />
+      </View>
+
+      <View className="mt-3 p-2">
+        <Text className="text-sm">Прізвище</Text>
+        <TextInput
+          className="border border-gray-300"
+          onChangeText={(text) => setLastName(text)}
+          value={lastName}
+        />
+      </View>
+      <View className="p-2">
+        <Text className="text-sm">Ім'я</Text>
+        <TextInput
+          className="border border-gray-300"
+          onChangeText={(text) => setFirstName(text)}
+          value={firstName}
+        />
+      </View>
+
+      <Button
+        title="Зареєструватися"
+        className="m-2 bg-blue-400 p-2"
+        onPress={() => {
+          console.log(
+            `Email: ${email}, Password: ${password}, Password Confirmation: ${passwordConfirmation}, Last Name: ${lastName}, First Name: ${firstName}`
+          );
+
+          setEmail('');
+          setPassword('');
+          setPasswordConfirmation('');
+          setLastName('');
+          setFirstName('');
+        }}>
+        Зареєструватися
+      </Button>
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
