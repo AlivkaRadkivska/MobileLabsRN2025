f3b694f9655d9d3b27f52197ec45b2cc7e99276c
AlivkaRadkivska <aradkivska@gmail.com>
2025-03-11 19:16:11 +0200

    feat: add header and footer

--- lab_1/app/_layout.tsx
+++ lab_1/app/_layout.tsx
@@ -1,17 +1,34 @@
-import "../global.css";
+import { Image, Text, View } from 'react-native';
+import '../global.css';
 
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(tabs)",
+  initialRouteName: '(tabs)',
 };
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="modal" options={{ presentation: "modal" }} />
-    </Stack>
+    <View className="flex-1">
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerTitle: () => (
+              <Image
+                className="h-16 w-32"
+                resizeMode="contain"
+                source={require('~/assets/ztu.png')}
+              />
+            ),
+            headerRight: () => <Text className="font-bold">FirstMobileApp</Text>,
+          }}
+        />
+      </Stack>
+      <View>
+        <Text className="text-center text-sm italic">Радківська Аліна Валеріївна, ВТ-21-1</Text>
+      </View>
+    </View>
   );
 }
