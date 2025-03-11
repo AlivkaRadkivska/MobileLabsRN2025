90fcdf1ed1a8907689e339403d31f3286da89c66
AlivkaRadkivska <aradkivska@gmail.com>
2025-03-11 19:36:50 +0200

    feat: add screen with news

--- lab_1/app/(tabs)/index.tsx
+++ lab_1/app/(tabs)/index.tsx
@@ -1,23 +1,31 @@
-import { Stack } from "expo-router";
+import { FlatList, Image, Text, View } from 'react-native';
 
-import { StyleSheet, View } from "react-native";
-
-import { ScreenContent } from "~/components/ScreenContent";
+const data = new Array(15).fill(null).map((_, i) => ({
+  id: i,
+  title: `Новина ${i + 1}`,
+  description: `Опис новини ${i + 1}`,
+  image: `https://picsum.photos/seed/${i}/80/80`,
+  date: new Date().toLocaleString(),
+}));
 
 export default function Home() {
   return (
-    <>
-      <Stack.Screen options={{ title: "Tab One" }} />
-      <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
-      </View>
-    </>
+    <View className="flex-1">
+      <FlatList
+        data={data}
+        ListHeaderComponent={<Text className="text-center text-xl font-semibold">Новини</Text>}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View className="flex flex-row gap-2 p-2">
+            <Image className="h-20 w-20 rounded-sm" source={{ uri: item.image }} />
+            <View>
+              <Text className="font-bold">{item.title}</Text>
+              <Text className="text-sm text-gray-500">{item.date}</Text>
+              <Text className="text-sm">{item.description}</Text>
+            </View>
+          </View>
+        )}
+      />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
