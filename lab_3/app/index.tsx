fc3943b5b09f31ff56f70d1030b846156a50b5b5
AlivkaRadkivska <aradkivska@gmail.com>
2025-03-27 19:29:22 +0200

    feat: add PanGestureHandler

--- lab_3/app/index.tsx
+++ lab_3/app/index.tsx
@@ -1,10 +1,24 @@
 import { Link } from 'expo-router';
 import { useState } from 'react';
 import { View } from 'react-native';
-import { LongPressGestureHandler, TapGestureHandler } from 'react-native-gesture-handler';
+import {
+  LongPressGestureHandler,
+  PanGestureHandler,
+  TapGestureHandler,
+} from 'react-native-gesture-handler';
+import Animated, { useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
 
 export default function Home() {
   const [score, setScore] = useState(0);
+
+  const oldTranslateX = useSharedValue(0);
+  const oldTranslateY = useSharedValue(0);
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ translateX: translateX.value }, { translateY: translateY.value }],
+  }));
 
   return (
     <View>
@@ -28,7 +42,24 @@ export default function Home() {
               onEnded={() => {
                 setScore((prev) => prev + 3);
               }}>
-              <View className="h-[150px] w-[150px] rounded bg-red-800" />
+              <PanGestureHandler
+                minDist={50}
+                onBegan={() => {
+                  oldTranslateX.value = translateX.value;
+                  oldTranslateY.value = translateY.value;
+                }}
+                onGestureEvent={(event) => {
+                  translateX.value = oldTranslateX.value + event.nativeEvent.translationX;
+                  translateY.value = oldTranslateY.value + event.nativeEvent.translationY;
+                }}
+                onEnded={() => {
+                  setScore((prev) => prev + 10);
+                }}>
+                <Animated.View
+                  className="h-[150px] w-[150px] rounded bg-red-800"
+                  style={animatedStyle}
+                />
+              </PanGestureHandler>
             </LongPressGestureHandler>
           </TapGestureHandler>
         </TapGestureHandler>
