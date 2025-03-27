ffc24e354b6db37f364afd33a28d27a2ed39763a
AlivkaRadkivska <aradkivska@gmail.com>
2025-03-27 19:19:26 +0200

    feat: add TapGestureHandler

--- lab_3/app/index.tsx
+++ lab_3/app/index.tsx
@@ -1,16 +1,31 @@
 import { Link } from 'expo-router';
+import { useState } from 'react';
 import { View } from 'react-native';
+import { TapGestureHandler } from 'react-native-gesture-handler';
 
 export default function Home() {
+  const [score, setScore] = useState(0);
+
   return (
     <View>
       <View>
         <Link className="z-50 text-center text-2xl font-bold text-blue-500 underline" href="/tasks">
-          Завдання
+          Завдання {score}
         </Link>
       </View>
       <View className="items-center justify-center">
-        <View className="h-[150px] w-[150px] rounded bg-red-800" />
+        <TapGestureHandler
+          onEnded={() => {
+            setScore((prev) => prev + 1);
+          }}>
+          <TapGestureHandler
+            numberOfTaps={2}
+            onEnded={() => {
+              setScore((prev) => prev + 2);
+            }}>
+            <View className="h-[150px] w-[150px] rounded bg-red-800" />
+          </TapGestureHandler>
+        </TapGestureHandler>
       </View>
     </View>
   );
