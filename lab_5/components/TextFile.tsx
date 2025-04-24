16dcd8166d7dad2d387d29814d8dab8a0b9a5758
AlivkaRadkivska <aradkivska@gmail.com>
2025-04-24 13:20:05 +0300

    feat: file update logic

--- lab_5/components/TextFile.tsx
+++ lab_5/components/TextFile.tsx
@@ -1,4 +1,4 @@
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import * as FileSystem from 'expo-file-system';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { rootPath } from '~/const';
@@ -14,12 +14,26 @@ export default function TextFile({
   name: string;
   refresh: () => void;
 }) {
+  const [isEditing, setIsEditing] = useState(false);
   const [stats, setStats] = useState<{
     size: string;
     lastModified: string;
   }>();
+  const [fileContent, setFileContent] = useState('');
 
   useEffect(() => {
+    if (!isEditing) {
+      setFileContent('');
+      return;
+    }
+    (async () => {
+      const content = await FileSystem.readAsStringAsync(`${rootPath}${fullPath}`);
+      setFileContent(content);
+    })();
+  }, [isEditing]);
+
+  useEffect(() => {
+    if (isEditing) return;
     (async () => {
       const fileInfo = await FileSystem.getInfoAsync(`${rootPath}${fullPath}`);
       if (fileInfo.exists) {
@@ -30,7 +44,11 @@ export default function TextFile({
         });
       }
     })();
-  }, []);
+  }, [isEditing]);
+
+  const handleUpdate = async () => {
+    await FileSystem.writeAsStringAsync(`${rootPath}${fullPath}`, fileContent);
+  };
 
   const handleDelete = async () => {
     await FileSystem.deleteAsync(`${rootPath}${fullPath}`, { idempotent: true });
@@ -38,22 +56,41 @@ export default function TextFile({
   };
 
   return (
-    <TouchableOpacity className="flex-row justify-between p-1">
-      <View className="flex-row items-center gap-2">
-        <FontAwesome name="file-o" size={44} color="gray" />
-        <View>
-          <Text>{name}</Text>
-          {stats && (
-            <View>
-              <Text className="text-xs">Size: {stats.size}</Text>
-              <Text className="text-xs">Last modified: {stats.lastModified}</Text>
-            </View>
-          )}
+    <>
+      <TouchableOpacity
+        className="flex-row justify-between p-1"
+        onPress={() => setIsEditing((prev) => !prev)}>
+        <View className="flex-row items-center gap-2">
+          <FontAwesome name="file-o" size={44} color="gray" />
+          <View>
+            <Text>{name}</Text>
+            {stats && (
+              <View>
+                <Text className="text-xs">Size: {stats.size}</Text>
+                <Text className="text-xs">Last modified: {stats.lastModified}</Text>
+              </View>
+            )}
+          </View>
         </View>
-      </View>
-      <TouchableOpacity onPress={handleDelete}>
-        <FontAwesome name="trash" size={34} color="red" />
+        <TouchableOpacity onPress={handleDelete}>
+          <FontAwesome name="trash" size={34} color="red" />
+        </TouchableOpacity>
       </TouchableOpacity>
-    </TouchableOpacity>
+      {isEditing && (
+        <View>
+          <TextInput
+            className="rounded-md border-2 border-gray-300 p-2"
+            placeholder="Enter file content"
+            value={fileContent}
+            onChangeText={setFileContent}
+            onSubmitEditing={() => {
+              handleUpdate();
+              setFileContent('');
+              setIsEditing(false);
+            }}
+          />
+        </View>
+      )}
+    </>
   );
 }
